71ed781fd250f6b7f97fa2d6d22030c9d43f23d1
Majid Ali <majidalipahore55566@gmail.com>
2024-06-21 09:06:37 -0700

    layouting project structure

---
+++ src/app/page.js
@@ -0,0 +1,6 @@
+import Image from "next/image";
+import "./styles/main.scss";
+
+export default function Home() {
+  return <h1>welcome home</h1>;
+}
---
+++ src/app/register/page.jsx
@@ -0,0 +1,3 @@
+export default function Regsiter() {
+  return <div>Sign up</div>;
+}
